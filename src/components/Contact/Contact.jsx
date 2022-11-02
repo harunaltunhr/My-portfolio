5c01945cf210fe15e8d0c8cc5295c4a5ea0b68ed
HarunAltun <altunhr@gmail.com>
2022-11-03 00:42:15 +0100

    Contact component created- emailjs adopted

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,87 @@
 import React from "react";
+import { useRef } from "react";
+import "./contact.css";
+import emailjs from "@emailjs/browser";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { SiWhatsapp } from "react-icons/si";
 
-export const Contact = () => {
-  return <section id="contact">Contact</section>;
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_m5vdzja",
+      "template_79kvvba",
+      form.current,
+      "GGsb5YanGvoyyEHBE"
+    );
+    e.target.reset();
+    alert("Message Sent");
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className=".contact__option-icon" />
+            <h4>Email</h4>
+            <h5>altunhr@gmail.com</h5>
+            <a href="mailto:altunhr@gmail.com" rel="noreferrer" target="_blank">
+              Send a massage
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaFacebookMessenger className=".contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Harun Altun</h5>
+            <a
+              href="https://m.me/harun.altun.58958"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Send a massage
+            </a>
+          </article>
+          <article className="contact__option">
+            <SiWhatsapp className=".contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+32499400302</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=32499400302"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a massage
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,6 +1,52 @@
 import React from "react";
 import "./portfolio.css";
 import MYPHOTO from "../../assests/MYPHOTO.jpeg";
+
+const data = [
+  {
+    id: 1,
+    image: MYPHOTO,
+    title: "First Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+  {
+    id: 2,
+    image: MYPHOTO,
+    title: "Second Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+  {
+    id: 3,
+    image: MYPHOTO,
+    title: "Third Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+  {
+    id: 4,
+    image: MYPHOTO,
+    title: "Fourth Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+  {
+    id: 5,
+    image: MYPHOTO,
+    title: "Fifth Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+  {
+    id: 6,
+    image: MYPHOTO,
+    title: "Sixth Project that I used React",
+    github: "https://github.com/harunaltunhr",
+    demo: "https://google.com",
+  },
+];
+
 const portfolio = () => {
   return (
     <section id="portfolio">
@@ -8,26 +54,24 @@ const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={MYPHOTO} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a
-            href="https://github.com/harunaltunhr"
-            className="btn"
-            target="blank"
-          >
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/altunhr"
-            className="btn btn-primary"
-            target="blank"
-          >
-            Live Demo
-          </a>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
